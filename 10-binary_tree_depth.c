b719cd75dac8f2905a1ec8c73490e7dfa4ca6229
Ahmed Hosny <hosny4620@gmail.com>
2024-02-12 22:52:34 +0200

    bb

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -9,8 +9,8 @@
 size_t binary_tree_depth(const binary_tree_t *tree){
     size_t d = 0;
 
-    if (!tree){
-        return;
+    if (tree == NULL){
+        return (0);
     }
     if(tree->parent)
     {
